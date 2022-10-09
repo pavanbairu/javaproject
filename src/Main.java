b548c01979921ab4c81566b4ca1b42b4398dcc70
pavanbairu <pavan.bairu1@gmail.com>
2022-10-09 13:10:54 +0530

    added addition arithmetic operations

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args)
     {
-        System.out.println("Hello Bairu Pavan");
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        System.out.println("addition of a & b :" +c);
+        System.out.println("Hello world");
     }
 }
